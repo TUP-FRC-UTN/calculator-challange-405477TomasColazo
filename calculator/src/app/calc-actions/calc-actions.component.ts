3f3d8696760dd3007049ee42171ed72e2493d145
405477TomasColazo <405477@tecnicatura.frc.utn.edu.ar>
2024-08-23 22:29:58 -0300

    Todas las operaciones funcionando

--- calculator/src/app/calc-actions/calc-actions.component.ts
+++ calculator/src/app/calc-actions/calc-actions.component.ts
@@ -12,9 +12,40 @@ export class CalcActionsComponent {
   @Input() n1: number = 0;
   @Input() n2: number = 0;
   @Output() ans = new EventEmitter<number>();
+  @Output() op = new EventEmitter<string>();
+  @Output() borrarEvent = new EventEmitter<void>();
 
+  borrar() {
+    this.borrarEvent.emit();
+  }
   sumar() {
     const x = this.n1 + this.n2;
     this.ans.emit(x);
+    const operacion = `${this.n1} + ${this.n2} = ${x}`;
+    this.op.emit(operacion);
+  }
+  restar() {
+    const x = this.n1 - this.n2;
+    this.ans.emit(x);
+    const operacion = `${this.n1} - ${this.n2} = ${x}`;
+    this.op.emit(operacion);
+  }
+  potenciar() {
+    const x = this.n1 ** this.n2;
+    this.ans.emit(x);
+    const operacion = `${this.n1} ^ ${this.n2} = ${x}`;
+    this.op.emit(operacion);
+  }
+  multiplicar() {
+    const x = this.n1 * this.n2;
+    this.ans.emit(x);
+    const operacion = `${this.n1} * ${this.n2} = ${x}`;
+    this.op.emit(operacion);
+  }
+  dividir() {
+    const x = this.n1 / this.n2;
+    this.ans.emit(x);
+    const operacion = `${this.n1} / ${this.n2} = ${x}`;
+    this.op.emit(operacion);
   }
 }
--- calculator/src/app/calc-view/calc-view.component.ts
+++ calculator/src/app/calc-view/calc-view.component.ts
@@ -5,19 +5,45 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-calc-view',
   standalone: true,
-  imports: [CalcActionsComponent],
+  imports: [CalcActionsComponent, FormsModule],
   templateUrl: './calc-view.component.html',
   styleUrl: './calc-view.component.css',
 })
 export class CalcViewComponent {
+  hayRespuesta: string = 'none';
   n1: number = 0;
   n2: number = 0;
   resultado: number = 0;
+  /*  operacion: string = ''; */
+  private _operacion: string = '';
+
+  set operacion(value: string) {
+    this._operacion = value;
+    this.mostrar();
+  }
+  get operacion(): string {
+    return this._operacion;
+  }
+
   updateValue1(value: string) {
     this.n1 = +value;
   }
 
   updateValue2(value: string) {
     this.n2 = +value;
+  }
+
+  mostrar() {
+    if (this.operacion == '') {
+      this.hayRespuesta = 'none';
+    } else {
+      this.hayRespuesta = 'block';
+    }
+  }
+  borrar() {
+    this.n1 = 0;
+    this.n2 = 0;
+    this.resultado = 0;
+    this.operacion = '';
   }
 }
